f18466a5800a5e8660d8a51d837ba43daca1e9d5
Oseid <eoseid@amazon.com>
2017-08-22 15:00:43 -0700

    fuck those changes ass hole

--- HelloGit.java
+++ HelloGit.java
@@ -3,8 +3,5 @@ import java.util.*;
 public class HelloGit {
 	public static void main(String[] args){
 		System.out.println("Hello Git");
-		System.out.println("This is a new change ma dude");
-		System.out.println("This is a third change");
-		System.out.println("this ia a nother change");
 	}
 }
