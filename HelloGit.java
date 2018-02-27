0da153cd3fe48319cf6a2975d3e2eeaec1e8cb6d
Oseid <eoseid@amazon.com>
2018-02-26 22:57:10 -0800

    this is my second idea

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HelloGit {
 	public static void main(String[] args){
-		System.out.println("This is my first idea");
+		System.out.println("This is my second idea");
 		System.out.println("This is a change");
 		System.out.println("a single change");
 	}
