3892f3f1ce5524a82abfea4a2357e858ba70d590
Oseid <eoseid@amazon.com>
2018-03-06 13:11:51 -0800

    Starting from scratch

--- HelloGit.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class HelloGit {
-	public static void main(String[] args){
-		System.out.println("This is my second idea");
-		System.out.println("This is a change");
-		System.out.println("a single change");
-	}
-}
